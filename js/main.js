c53d47bc74bf7eaad49fabbe78ecfcc909175bd4
Byt1k <byt1kip82@gmail.com>
2022-05-08 23:04:37 +0300

    added homepage (tablet version)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 $('.mainSlider').owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
     dots: true,
     items: 1,
     navText: [
         '<img src="img/carousel-prev.svg" alt="prev" />',
         '<img src="img/carousel-next.svg" alt="next" />'
-    ]
+    ],
+    responsive: {
+        0: {
+            nav: false
+        },
+        992: {
+            nav: true
+        }
+    }
+
 })
 
 $('.first-slider').owlCarousel({
@@ -29,11 +37,19 @@ $('.second-slider').owlCarousel({
 $('.popularRow1').owlCarousel({
     loop: true,
     margin: 23,
-    nav: true,
     dots: false,
     responsive: {
         1200: {
             items: 4
+        },
+        992: {
+            nav: true
+        },
+        768: {
+            items: 3
+        },
+        0: {
+            nav: false
         }
     },
     navText: [
@@ -45,12 +61,21 @@ $('.popularRow1').owlCarousel({
 $('.popularRow2').owlCarousel({
     loop: true,
     margin: 23,
-    nav: true,
     dots: false,
     responsive: {
         1200: {
             items: 4
+        },
+        992: {
+            nav: true
+        },
+        768: {
+            items: 3
+        },
+        0: {
+            nav: false
         }
+        
     },
     navText: [
         '<img src="img/carousel-prev.svg" alt="prev" />',
