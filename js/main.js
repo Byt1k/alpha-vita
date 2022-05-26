ce38c29e87a702ed2b703450300c2b92c21f5074
Byt1k <byt1kip82@gmail.com>
2022-05-26 23:48:10 +0300

    added certificates page

--- js/main.js
+++ js/main.js
@@ -82,3 +82,70 @@ btnMenuClose.addEventListener('click', () => {
 })
 
 
+$('.certificates-wrapper').owlCarousel({
+    center: true,
+    loop: true,
+    nav: true,
+    margin: 0,
+    dots: false,
+    navText: [
+        '<img src="img/carousel-prev.svg" alt="prev" />',
+        '<img src="img/carousel-next.svg" alt="next" />'
+    ],
+    responsive:{
+        1200: {
+            items: 5,
+            nav: true
+        },
+        992: {
+            items: 4,
+            nav: false
+        },
+        768: {
+            nav: false,
+            items: 3
+        },
+        0: {
+            nav: false,
+            loop: false
+        }
+    }
+});
+
+
+const certificatesZoomBtns = document.querySelectorAll('.certificates-wrapper .item .zoom');
+
+certificatesZoomBtns.forEach((btn) => {
+    btn.onclick = (e) => {
+        const imgSrc = e.target.parentNode.parentNode.querySelector('img').src;
+        let overlay = document.createElement('div')
+        overlay.className = 'overlay';
+        overlay.innerHTML = '<div class="magnify"><img src="'+imgSrc+'"><div class="close-popup"><i></i></div></div>';
+        
+        const body = document.querySelector('body');
+        body.append(overlay);
+        body.style.overflowY = 'hidden';
+        
+        body.onclick = (e) => {
+            if (e.target !== (document.querySelector('.overlay .magnify img')) && 
+                e.target.className !== 'zoom' &&
+                e.target.className !== 'increase'
+            ) {
+                overlay.remove();
+                body.style.overflowY = 'visible';
+            }
+        }
+
+    }
+})
+
+$(window).resize(function(){
+    if($(window).width() > 768){
+        $('.certificates-wrapper').trigger('destroy.owl.carousel');
+    } else {
+        
+    }
+});
+
+
+
